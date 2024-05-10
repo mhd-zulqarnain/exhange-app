6bb282114ab1e71685cfe0c494a507709018a25d
Zulqarnain <mhd.zeeq@gmail.com>
2024-05-10 07:44:38 +0500

    resloved all dependency issues

---
+++ app/src/main/java/com/application/exchange/ui/compose/HomeActivity.kt
@@ -0,0 +1,50 @@
+package com.application.exchange.ui.compose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import dagger.hilt.android.AndroidEntryPoint
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.application.exchange.ui.MainViewModel
+
+@AndroidEntryPoint
+class HomeActivity : ComponentActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ExchangeTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier, viewModel: HomeViewModel = viewModel()) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ExchangeTheme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/com/application/exchange/ui/compose/HomeViewModel.kt
@@ -0,0 +1,98 @@
+package com.application.exchange.ui.compose
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.application.core.Result
+import com.application.core.data.local.ExchangeEntity
+import com.application.core.data.model.CoroutinesDispatcherProvider
+import com.application.core.exchange.ExchangeRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+
+@HiltViewModel
+class HomeViewModel @Inject constructor(
+    private val exchangeRepository: ExchangeRepository,
+    private val dispatcherProvider: CoroutinesDispatcherProvider,
+) : ViewModel() {
+    private var cacheExchangeList: List<ExchangeEntity>? = null
+
+    private val _exchangeCurrency = MutableLiveData<ExchangeResult>()
+    val exchangeCurrency: LiveData<ExchangeResult>
+        get() = _exchangeCurrency
+
+    fun setStateEvent(event: ExchangeStateEvent) {
+        when (event) {
+            ExchangeStateEvent.FetchExchangeRate -> {
+                fetchExchangeRateFlow()
+            }
+
+            is ExchangeStateEvent.ConvertAmount -> {
+                exchangeAmount(event.amount,event.selectedCurrencyUSD)
+            }
+
+            is ExchangeStateEvent.ValidateConversion -> {
+                convertButtonValidation(event.amount, event.selectedCurrency)
+            }
+        }
+
+    }
+
+    private fun convertButtonValidation(amount: String, selectedCurrency: ExchangeEntity?) {
+        _exchangeCurrency.postValue(ExchangeResult.ValidateConversion(amount.isNotEmpty() && selectedCurrency != null))
+    }
+
+    private fun fetchExchangeRateFlow() = viewModelScope.launch(dispatcherProvider.io) {
+        // can be launched in a separate asynchronous job
+        exchangeRepository.getExchangeRate().onEach { result ->
+            withContext(dispatcherProvider.main) {
+                when (result) {
+                    is Result.Success -> {
+                        cacheExchangeList = result.data
+                        _exchangeCurrency.postValue(ExchangeResult.ExchangeRateResult(result))
+                    }
+                    is Result.Loading -> {
+                        _exchangeCurrency.postValue(ExchangeResult.ExchangeRateResult(Result.Loading))
+                    }
+
+                    else -> {
+                        _exchangeCurrency.postValue(ExchangeResult.ExchangeRateResult(result))
+                    }
+                }
+            }
+        }.launchIn(viewModelScope)
+    }
+
+    private fun exchangeAmount(amount: Double ,selectedCurrencyUSD:Double) {
+        cacheExchangeList?.let { list ->
+            list.forEach {
+                it.convertedAmount = it.getConvertedAmount(amount,selectedCurrencyUSD)
+            }
+            _exchangeCurrency.postValue(ExchangeResult.ExchangeRequestResult(list))
+        }
+    }
+
+    fun clearCache() {
+        cacheExchangeList = null
+        exchangeRepository.clearCache()
+        fetchExchangeRateFlow()
+    }
+}
+
+sealed class ExchangeResult {
+    data class ExchangeRateResult(val result: Result<List<ExchangeEntity>>) : ExchangeResult()
+    data class ExchangeRequestResult(val result: List<ExchangeEntity>) : ExchangeResult()
+    data class ValidateConversion(val result: Boolean) : ExchangeResult()
+}
+
+sealed class ExchangeStateEvent {
+    object FetchExchangeRate : ExchangeStateEvent()
+    class ConvertAmount(val amount: Double,val selectedCurrencyUSD: Double) : ExchangeStateEvent()
+    class ValidateConversion(val amount: String, val selectedCurrency: ExchangeEntity?) :
+        ExchangeStateEvent()
+}
