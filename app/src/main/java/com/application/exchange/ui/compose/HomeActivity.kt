cfa88a81c33cf7bafdfa038ebdcd4fd058ed3179
Zulqarnain <mhd.zeeq@gmail.com>
2024-05-11 08:26:31 +0500

    resloved all dependency issues

--- app/src/main/java/com/application/exchange/ui/compose/HomeActivity.kt
+++ app/src/main/java/com/application/exchange/ui/compose/HomeActivity.kt
@@ -4,14 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material3.Button
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
@@ -20,19 +19,20 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
 import dagger.hilt.android.AndroidEntryPoint
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.application.exchange.R
-import com.application.exchange.ui.ExchangeStateEvent
 
 @AndroidEntryPoint
 class HomeActivity : ComponentActivity() {
@@ -54,11 +54,16 @@ class HomeActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(modifier: Modifier = Modifier, viewModel: HomeViewModel = viewModel()) {
-    var text by remember { mutableStateOf("d") }
+    var text by remember { mutableStateOf("") }
     var selectedCurrency by remember { mutableStateOf("USD") }
-    val openDialog : () -> Unit= {
-        ConversionList()
+    val showDialog = remember { mutableStateOf(false) }
+
+    val openDialog: () -> Unit = {
+        showDialog.value = true
     }
+
+    if (showDialog.value) ShowCurrencies(showDialog, selectedCurrency)
+
     Column(
         modifier = modifier
             .fillMaxWidth()
@@ -71,9 +76,6 @@ fun Greeting(modifier: Modifier = Modifier, viewModel: HomeViewModel = viewModel
             OutlinedTextField(
                 value = text,
                 onValueChange = { text = it },
-                modifier=modifier.clickable {
-                    openDialog.invoke()
-                },
                 label = {
                     Text(
                         stringResource(id = R.string.enter_the_amount_the_convert),
@@ -89,46 +91,34 @@ fun Greeting(modifier: Modifier = Modifier, viewModel: HomeViewModel = viewModel
                 Text(text = "Convert")
             }
         }
-
-    }
-
-}
-
-
-
-@Composable
-fun ConversionList( viewModel: HomeViewModel = viewModel()) {
-    var text by remember { mutableStateOf("d") }
-    Column(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(16.dp)
-    ) {
 
         Row(
             verticalAlignment = Alignment.CenterVertically
         ) {
-            OutlinedTextField(
-                value = text,
-                onValueChange = { text = it },
-                label = {
-                    Text(
-                        stringResource(id = R.string.enter_the_amount_the_convert),
-                        color = Color.Black
-                    )
-                }
-            )
-            Spacer(modifier = Modifier.weight(1f))
-            Button(
+
+            ClickableText(
+                text = AnnotatedString("text"),
                 onClick = {
-                    viewModel.fetchExchangeRateFlow()
-                }) {
-                Text(text = "Convert")
-            }
+                    openDialog.invoke()
+                    text = "Disabled"
+                })
         }
 
     }
+
 }
+
+@Composable
+fun ShowCurrencies(showDialog: MutableState<Boolean>, selectedCurrency: String) {
+    Dialog(
+        onDismissRequest = { showDialog.value = false },
+    ) {
+        Surface() {
+            Text(text = "its a text view")
+        }
+    }
+}
+
 
 @Preview(showBackground = true)
 @Composable
